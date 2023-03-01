27bf96f2cb44d99d294f0d2fdb07602c26131c71
Dmitrii Builin <d.builin@mail.ru>
2023-03-01 17:15:19 +0200

    Les 2 started

--- src/Blog/Comment.php
+++ src/Blog/Comment.php
@@ -2,32 +2,92 @@
 
 namespace GeekBrains\LevelTwo\Blog;
 
-use GeekBrains\LevelTwo\Person\Person;
 use GeekBrains\LevelTwo\Blog\Post;
+use GeekBrains\LevelTwo\Blog\User;
 
 class Comment
 {
     private int $id;
-    private Person $author;
-    private int $postId;
+    private User $user;
+    private Post $post;
     private string $text;
-    // private int $postId = $post->id();
-    
-    
 
     public function __construct(
         int $id,
-        Person $author,
-        int $postId,
+        User $user,
+        Post $post,
         string $text
     )
     {
         $this->id = $id;
-        $this->author = $author;        
-        $this->postId = $postId; 
+        $this->user = $user;        
+        $this->post = $post; 
         $this->text = $text;
     }
 
+        /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return User
+     */
+    public function getUser(): User
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param User $user
+     */
+    public function setUser(User $user): void
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * @return Post
+     */
+    public function getPost(): Post
+    {
+        return $this->post;
+    }
+
+    /**
+     * @param Post $post
+     */
+    public function setPost(Post $post): void
+    {
+        $this->post = $post;
+    }
+
+    /**
+     * @return string
+     */
+    public function getText(): string
+    {
+        return $this->text;
+    }
+
+    /**
+     * @param string $text
+     */
+    public function setText(string $text): void
+    {
+        $this->text = $text;
+    }
     
     public function __toString()
     {
--- src/Blog/Post.php
+++ src/Blog/Post.php
@@ -2,31 +2,70 @@
 
 namespace GeekBrains\LevelTwo\Blog;
 
-use GeekBrains\LevelTwo\Person\Person;
+use GeekBrains\LevelTwo\Blog\User;
 
 class Post
 {
     private int $id;
-    private Person $author;
+    private User $user;
     private string $title;
-    private string $text;
-
-
-    
+    private string $text;    
 
     public function __construct(
         int $id,
-        Person $author,
+        User $user,
         string $title,
         string $text
     )
     {
         $this->id = $id;
-        $this->author = $author;
+        $this->user = $user;
         $this->title = $title;
         $this->text = $text;        
         
         // var_dump($author);
+    }
+
+        /**
+     * @param User $user
+     */
+    public function setUser(User $user): void
+    {
+        $this->user = $user;
+    }
+
+        /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle(string $title): Post
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getText(): string
+    {
+        return $this->text;
+    }
+
+    /**
+     * @param string $text
+     */
+    public function setText(string $text): Post
+    {
+        $this->text = $text;
+        return $this;
     }
 
     public function __toString()
